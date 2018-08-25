f3d68f5e9ce77ec0c41724f9dfff0004d85e9aab
dmtzbsz1997 <mzc258369@gmail.com>
2018-08-26 02:13:46 +1000

    Week2Review

--- SetNumberCounter.java
+++ SetNumberCounter.java
@@ -3,27 +3,24 @@ import java.util.ArrayList;
 public class SetNumberCounter {
 	public static void main(String[] args) {
 		Scanner keyboard= new Scanner(System.in);
-		try {
-			for(int i=0;keyboard.hasNextLine();i++) {
-				String s=keyboard.nextLine();
-				String[] str=s.split(" ");
-				for(int j=0; j<str.length; j++) {
-					System.out.println("set "+j);
-					for(int l=0;l<10;l++) {
-						int counter=0;
-						String number=""+l;
-						for(int k=0;k<str[j].length();k++) {
-							if(number.equals(""+str[j].charAt(k))) {
-								counter++;
-							}
+		for(int i=0;keyboard.hasNextLine();i++) {
+			String s=keyboard.nextLine();
+			String[] str=s.split(" ");
+			//assign input values into String array str
+			for(int j=0; j<str.length; j++) {
+				//go through string array str
+				System.out.println("set "+j);
+				for(int l=0;l<10;l++) {
+					int counter=0;
+					String number=Integer.toString(l);
+					for(int k=0;k<str[j].length();k++) {
+						if(number.equals(Character.toString(str[j].charAt(k)))) {
+							counter++;
 						}
-						System.out.println("Number of time "+ l+" Occurs: "+ counter);
-					}	
-				}
-				
+					}
+					System.out.println("Number of time "+ l+" Occurs: "+ counter);
+				}			
 			}
-		}catch(Exception e) {
-			System.out.println("Opps, Wrong input");
 		}		
 	}
 }
